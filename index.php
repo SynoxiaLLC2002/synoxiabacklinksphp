91078729146b9161d32cbbc55fa86c75168ec4b5
SynoxiaLLC2002 <136620641+SynoxiaLLC2002@users.noreply.github.com>
2023-06-14 22:29:49 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: grey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+button {
+  margin-top: 20px;
+}
+</style>
+</head>
+<body>
+  <h1> What is <a href="https://iskills.com/">Iskills</a> ?</h1>
+  <p> An institute by tanver nandla for seo & blogging </p>
+  <h2>Supportive System</h2>
+  <p>Our world-class support system includes mentorship, an online community and ongoing support to ensure the success of our students.</p>
+  <h2>Trusted Content</h2>
+  <p>Our training content is trusted and up-to-date, as it is developed and reviewed by industry experts who are passionate about staying current with the latest trends and best practices.</p>
+  <h1>Are you looking for cloud stacking backlinks?</h1>
+  <button style="background-color: #0000FF; color: white; font-size: 20px; padding: 10px 20px;" onclick="window.open('https://www.facebook.com/rehanmubarak32/')">Contact Me</button>
+
+  <?php
+  // PHP code goes here if needed
+  ?>
+</body>
+</html>
